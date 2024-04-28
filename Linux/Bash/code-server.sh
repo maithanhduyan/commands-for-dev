bf0919723abbdedfa99ebd71d630d2fe57b765e2
Mai Thành Duy An <tiachop0102@gmail.com>
2024-04-28 20:10:35 +0900

    [Linux] [Bash] Update install code-server.

--- Linux/Bash/code-server.sh
+++ Linux/Bash/code-server.sh
@@ -2,42 +2,31 @@
 
 # Trên Ubuntu, Debian chuẩn bị
 sudo apt-get install -y \
+  curl \
+  git \
   build-essential \
   pkg-config \
   python3
-  
-# 2024-04-28 NOT USE ------------------------------------------------------------
-# # cài nodejs version 18
-# # echo 'Cài đặt Node Version Manager - nvm'
-# curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.7/install.sh | bash
 
-# export NVM_DIR="$([ -z "${XDG_CONFIG_HOME-}" ] && printf %s "${HOME}/.nvm" || printf %s "${XDG_CONFIG_HOME}/nvm")"
-# [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh" # This loads nvm
- 
-# nvm install 18
-# # Sau khi chạy đoạn script: 'nvm install 18' mặc định sẽ sử dụng nodejs version 18.
-# # Để chắc chắn hệ thống đang sử dụng version 18 hay chưa thì dùng
-# nvm use 18
-# 2024-04-28 NOT USE ------------------------------------------------------------
-
+mkdir -p /home/coder/code-server
 # Clone mã nguồn code-server
 git clone https://github.com/coder/code-server /home/coder/code-server
-# echo 'OK'
+
+echo "Tải source code-server hoàn tất"
 
 # Di chuyển vào thư mục code-server
 cd /home/coder/code-server
 
-# Cài yarn
-# npm install --global yarn
+sh install.sh
 
-# Cài đặt code-server
-npm install -g code-server
+echo "Đã cài đặt code-server hoàn tất."
 
 # Khởi động code-server
-code-server 
+#code-server 
 
 # Khởi động code-server không password
-code-server --auth none
+#code-server --auth none
 
 # Khởi động code-server cho phép user truy cập từ internet (all IP)
-code-server --bind-addr=0.0.0.0:8080
+#code-server --bind-addr=0.0.0.0:8080
+# 099ef5d578f69473ba5ac35f
