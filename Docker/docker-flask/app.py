3fa91c43ac057b33f217bb9645248d0d11a6c46e
Mai Thành Duy An <tiachop0102@gmail.com>
2024-07-09 00:15:59 +0900

    [Docker] Update Flask project.

--- Docker/docker-flask/app.py
+++ Docker/docker-flask/app.py
@@ -1,7 +1,8 @@
 import time
 
 import redis
-from flask import Flask
+from flask import Flask, render_template
+from datetime import datetime
 
 app = Flask(__name__)
 cache = redis.Redis(host='redis', port=6379)
@@ -17,7 +18,48 @@ def get_hit_count():
             retries -= 1
             time.sleep(0.5)
 
-@app.route('/')
+@app.route('/count')
 def hello():
     count = get_hit_count()
     return 'Hello World! I have been seen {} times.\n'.format(count)
+
+@app.route('/')
+@app.route('/home')
+def home():
+    """Renders the home page."""
+    return render_template(
+        'index.html',
+        title='Home Page',
+        year=datetime.now().year,
+    )
+
+@app.route('/contact')
+def contact():
+    """Renders the contact page."""
+    return render_template(
+        'contact.html',
+        title='Contact',
+        year=datetime.now().year,
+        message='Your contact page.'
+    )
+
+@app.route('/about')
+def about():
+    """Renders the about page."""
+    return render_template(
+        'about.html',
+        title='About',
+        year=datetime.now().year,
+        message='Your application description page.'
+    )
+
+# from os import environ
+# # from FlaskWebProject import app
+
+# if __name__ == '__main__':
+#     HOST = environ.get('SERVER_HOST', '0.0.0.0')
+#     try:
+#         PORT = int(environ.get('SERVER_PORT', '5000'))
+#     except ValueError:
+#         PORT = 5000
+#     app.run(HOST, PORT)
