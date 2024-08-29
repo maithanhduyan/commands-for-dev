023c618fd59d92498f05af71e88b876b200f242f
Mai Thành Duy An <tiachop0102@gmail.com>
2024-08-30 00:39:39 +0900

    [Javascript] Ionic + Vue

---
+++ Javascript/ionic/ionic-vue/capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'ionic.vue',
+  appName: 'ionic-vue',
+  webDir: 'dist'
+};
+
+export default config;
