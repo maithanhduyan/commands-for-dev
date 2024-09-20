726c939a6a9bae3586d054d856ee3a0463269976
Mai Thành Duy An <tiachop0102@gmail.com>
2024-09-20 19:34:45 +0900

    [Project] Ticket update.

--- Projects/ticket/src/main/java/com/hot/ticket/controller/ServiceController.java
+++ Projects/ticket/src/main/java/com/hot/ticket/controller/ServiceController.java
@@ -2,11 +2,15 @@ package com.hot.ticket.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.Authentication;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import com.hot.ticket.model.Service;
 import com.hot.ticket.repository.ServiceRepository;
 
 @Controller
@@ -18,15 +22,28 @@ public class ServiceController {
     // Màn hình lấy số
     @GetMapping({ "/", "" })
     public String show(Authentication authentication, Model model) {
-        // User user ;
+        UserDetails userDetails = (UserDetails) authentication.getPrincipal();
+        
+        model.addAttribute("services", serviceRepository.findByOrganization("ACB").get());
+        return "org/service/table-service";
+    }
+
+    // http://localhost:8080/services/my-services
+    @GetMapping("/my-services")
+    public String showMyServices(Model model) {
         model.addAttribute("services", serviceRepository.findByOrganization("ACB").get());
         return "org/service/services";
     }
 
-    // Màn hình lấy số
     @GetMapping("/add")
     public String addService(Authentication authentication, Model model) {
-        model.addAttribute("services", serviceRepository.findAll());
-        return "/org/service/add-new-service";
+        model.addAttribute("service", new Service());
+        return "org/service/add-new-service";
+    }
+
+    @PostMapping("/add")
+    public String addNewService(@ModelAttribute Service service) {
+        serviceRepository.save(service);
+        return "redirect:/service/my-services";
     }
 }
